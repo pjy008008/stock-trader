35ef60e3686954e0ec0bbd809a7289a80eb7fdb2
Junyou Park <pjy008008@gmail.com>
2025-12-17 18:28:14 +0900

    refactor: agent 에이전트 리팩토링
    역할을 나타내는 이름을 디렉토리에 명시하여 리팩토링

--- time_agent/agent.py
+++ time_agent/agent.py
@@ -7,7 +7,7 @@ def get_current_time(city: str) -> dict:
 
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
+    name='time_agent',
     description="Tells the current time in a specified city.",
     instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
     tools=[get_current_time],
