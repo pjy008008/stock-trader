ff868125be6cc09c2b01857389351358b6381b43
Junyou Park <pjy008008@gmail.com>
2025-12-18 18:20:47 +0900

    feat(tool_agent): 도구 활용 agent 생성
    agent가 도구를 활용해서 결과를 도출 할 수 있도록 개발
    - get_current_time: 파이썬 함수 형태로 현재 시간을 반환하는 도구
    - google_search: 구글의 검색기능을 활용하는 도구

---
+++ tool_agent/agent.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from google.adk.agents.llm_agent import Agent
+from google.adk.tools import google_search
+
+def get_current_time() -> dict:
+    """
+    Get the current time in the format YYYY-MM-DD HH:MM:SS
+    """
+    return {
+        "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+
+root_agent = Agent(
+    model='gemini-2.5-flash-preview-09-2025',
+    name='tool_agent',
+    description="Greeting agent",
+    instruction="""
+    You are a helpful assistant that can use the following tools:
+    - get_current_time
+    """,
+    # tools=[google_search],
+    tools=[get_current_time]
+)
